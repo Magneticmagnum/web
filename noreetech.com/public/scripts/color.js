92b14d844e1bb18e243029c0081f5e6c6e2679e4
magneticmagnum <magnum@noreetech.com>
2012-08-13 15:25:10 -0500

    Script to change stick colors!

--- noreetech.com/public/scripts/color.js
+++ noreetech.com/public/scripts/color.js
@@ -1,20 +1,38 @@
 var xbox360wirelesshtml = "file:///C:/Users/Virus/My%20Documents/Aptana%20Studio%203%20Workspace/noreetech.com/public/colorxbox360wireless.html#";
+/*
+ * palette to colored image class / id links:
+ * 
+ * palette:				image:
+ * #left-stick			xboxleftstick
+ * #right-stick			xboxrightstick
+ * #x-button			xboxbuttonx
+ * #y-button			xboxbuttony
+ * #a-button			xboxbuttona
+ * #b-button			xboxbuttonb
+ */
+function changeButtonImage(className, col){
+	var classToChange = ".xbox" +className;
+	var parent = $(classToChange).parent();
+	$(classToChange).remove();
+	$(parent).append('<img src="images/stick images/xbox-360-controller-stick-' + col + '.png" class="xbox' + className + '">');
+	alert(parent);
+}
 
 $(document).ready(function() {
 	$(".hide").hide();
+	
+	
+});
 
-	/*
-	 * Checks cookies for model cookie.  If it's not found, create one.
-	 * If 'model' cookie found, redirect to correct html page
-	 */
-	if($.cookie("model") == null){
-		$.cookie("model", "xbox 360 wireless");
+$(".colorpalette img").click(function() {
+	var myColor = $(this).parent().attr("class");
+	if ($(this).parent().siblings().children().hasClass("checkbox")) {
+		$(this).parent().siblings().children().remove(".checkbox");
+		$(this).parent().append('<img src="images/stick images/paletteX.png" class="checkbox">');
 	}
-	else{
-		var myModel = $.cookie("model");
-		switch(myModel){
-			case "xbox 360 wireless":
-				window.location.replace(xbox360wirelesshtml);
-		}
-	}
-}); 
+	var thisItem = $(this).closest("ul").attr("id");
+	changeButtonImage(thisItem, myColor);
+
+});
+
+
